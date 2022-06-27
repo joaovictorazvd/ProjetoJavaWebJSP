367c5bde8ef810bce97cc7a5b3b7efca3ae1ee2f
joaovictorazvd <105984731+joaovictorazvd@users.noreply.github.com>
2022-06-26 22:45:57 -0300

    modificando index e criando validações no servlet para não ser possível colocar o login e a senha vazios

--- curso-jsp/src/main/java/servlets/ServletLogin.java
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -2,25 +2,38 @@ package servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
 
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public ServletLogin() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletLogin() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("idade"));
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String login = request.getParameter("nome");
+		String senha = request.getParameter("idade");
+
+		if ((login != null && !login.isEmpty()) && (senha != null && !senha.isEmpty())) {
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+		}
+		else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e a senha corretamente");
+			redirecionar.forward(request, response);
+		}
+	}
 }
--- curso-jsp/src/main/webapp/index.jsp
+++ curso-jsp/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,16 +9,24 @@
 
 <h1>Bem vindo ao curso de JSP</h1>
 <body>
+	<form action="ServletLogin" method="post">
+		<table>
+			<tr>
+				<td><label>Login</label></td>
+				<td><input name="login" type="text"></td>
+			</tr>
 
+			<tr>
+				<td><label>Senha</label></td>
+				<td><input name="senha" type="password"></td>
+			</tr>
 
-<form action="ServletLogin" method="post"> 
-
-<input name = "nome">
-<input name = "idade">
-
-<input type = "submit" value = "Enviar">
-
-</form>
-
+			<tr>
+				<td />
+				<td><input type="submit" value="Enviar"></td>
+			</tr>
+		</table>
+	</form>
+	<h4>${msg}</h4>
 </body>
 </html>
